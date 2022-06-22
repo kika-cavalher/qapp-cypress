e81ed9d5159924b32623485bb1c951b33c2c88c0
kika-cavalher <ericavalher@gmail.com>
2022-06-21 22:08:16 -0300

    inicialized

---
+++ cypress/e2e/qapp.cy.js
@@ -0,0 +1,10 @@
+const site = 'https://qapp-web.vercel.app/'
+const TextLoginHome = 'Faça o seu login.'
+
+describe('Home page', () => {
+  it('App must be online', () => {
+    cy.viewport(1920,1080)
+    cy.visit(site)
+    cy.get('h1').should('have.text', TextLoginHome)
+  })
+})
